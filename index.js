a083260cf1f2399c217dfb2c4d700ac6c88fe9ba
AnikHassan111 <miaanik4@gmail.com>
2023-10-21 23:19:06 +0600

    get username and password .env file

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ app.get('/', (req, res) => {
 })
 
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wnl4pp8.mongodb.net/?retryWrites=true&w=majority`;
-const uri = `mongodb+srv://assigement10:6QtPjsGK65_cCc_@cluster0.wnl4pp8.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wnl4pp8.mongodb.net/?retryWrites=true&w=majority`;
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
